52c8c0de717ee410b6219143eb5c34e69de26866
Stiliyan Bakalov <altazsazn.basizila@gmail.com>
2026-05-07 13:18:36 +0300

    Fixes

--- js/home.js
+++ js/home.js
@@ -15,19 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function updateClock() {
-        const clockElement = document.getElementById('live-clock');
-        if (clockElement) {
-            const now = new Date();
-            const hours = String(now.getHours()).padStart(2, '0');
-            const minutes = String(now.getMinutes()).padStart(2, '0');
-            const seconds = String(now.getSeconds()).padStart(2, '0');
-            
-            clockElement.textContent = `${hours}:${minutes}:${seconds}`;
-        }
-    }
-    setInterval(updateClock, 1000);
-    updateClock();
+
 
     const physicsFacts = [
         "При равноускорително движение ускорението е постоянна величина с положителен знак.",
